d525eb236f748bf11f61899a315633ce47550a7e
Gustave.Marcus van Dorland <gustavedorland19@gmail.com>
2026-01-13 13:56:30 +0100

    opdracht 10

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\ProjectController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.layoutpublic');
 })->name('home');
 
 Route::view("admin", "layouts.layoutadmin")->name("admin");
